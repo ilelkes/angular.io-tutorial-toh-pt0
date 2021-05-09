b0b6bbccf0dcaf1f9ca3161920bee33bd43012fb
Lelkes István <47304380+ilelkes@users.noreply.github.com>
2021-05-09 21:38:46 +0200

    Routable HeroDetailComponent

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,3 +1,8 @@
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
+
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
 
@@ -9,9 +14,24 @@ import { Hero } from '../hero';
 export class HeroDetailComponent implements OnInit {
   @Input() hero?: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+  
+  goBack(): void {
+    this.location.back();
   }
 
 }
